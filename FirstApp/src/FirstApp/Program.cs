fa0ab82a1718a57311f116af7966b24f54a3c064
Rafa³ Kucharski <r.kucharski@lgbs.pl>
2016-12-26 14:33:49 +0100

    Loot at .netcore basic structure

---
+++ FirstApp/src/FirstApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace FirstApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // caly setup odbywa sie pdoczas startu serwera
+            // to jest setup request pipelinu       
+            var host = new WebHostBuilder()
+                .UseKestrel() // ten serwer jest uniwersalny dla wszystkich srodowisk
+                .UseContentRoot(Directory.GetCurrentDirectory()) 
+                // content root to sciezka dla serwera a webroot jest okreslony pod referencjami
+
+                .UseIISIntegration() // to ustawia IIS uzywany w visual studio jako default - jako reverse proxy
+                .UseStartup<Startup>() // dodaje do pipa zawartosc startup
+                .Build();
+
+            host.Run();
+        }
+    }
+}
